b1262f22c04ee0b6fde5428944305a004196ccbb
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-07-03 12:38:36 +0300

    add other files

--- js/main.js
+++ js/main.js
@@ -7,6 +7,28 @@ $(document).ready(function () {
         $('.header__bottom-menu').toggleClass('active');
     });
 
+    // mobile menu --------- //
+    $('.header__bottom-menu a').on('click', function () {
+        $('.header__bottom-menu').removeClass('active');
+        $('.burger-btn').toggleClass('active');
+    });
+
+    $(".scrolllink").on("click", 'a', function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top - 130;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
+
     // accordion --------- //
     $('.question__accordeon-header').on('click', function () {
         $(this).toggleClass('active');
@@ -14,12 +36,5 @@ $(document).ready(function () {
     });
 
 
-    // scroll top
-    $('.footer__btn').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 0);
-        return false;
-    });
 
 });
